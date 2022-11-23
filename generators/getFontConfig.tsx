08a4a7391e8b0a41d0d699531181f75339b3c3a0
Nathan Pate <101912488+nathanpate@users.noreply.github.com>
2022-11-23 10:39:15 +0800

    Update getFontConfig.tsx

--- generators/getFontConfig.tsx
+++ generators/getFontConfig.tsx
@@ -24,7 +24,7 @@ export const getFontConfig = (
   if (arrayCheck(preloadConfig)) {
     preloadConfig.forEach(href => {
       headComponents.push(
-        <link key={`preload-${href}`} rel="stylesheet" href={href} />
+        <link key={`preload-${href}`} rel="preload" as="style" href={href} />
       )
     })
   }
